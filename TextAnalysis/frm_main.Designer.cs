00eb7b1445df637dc8ae1c68d0e3eb31157e39a0
lxepoo <lxepoo@vip.qq.com>
2017-03-10 18:10:52 +0800

    已打包完成

---
+++ TextAnalysis/frm_main.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace TextAnalysis
+{
+    partial class frm_main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_main));
+            this.pannel_text = new System.Windows.Forms.FlowLayoutPanel();
+            this.btn_read = new System.Windows.Forms.Button();
+            this.lab_title = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tip_text = new System.Windows.Forms.ToolTip(this.components);
+            this.lab_count = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pannel_text
+            // 
+            this.pannel_text.AutoScroll = true;
+            this.pannel_text.Location = new System.Drawing.Point(13, 91);
+            this.pannel_text.Name = "pannel_text";
+            this.pannel_text.Size = new System.Drawing.Size(894, 496);
+            this.pannel_text.TabIndex = 0;
+            // 
+            // btn_read
+            // 
+            this.btn_read.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_read.Location = new System.Drawing.Point(761, 12);
+            this.btn_read.Name = "btn_read";
+            this.btn_read.Size = new System.Drawing.Size(139, 55);
+            this.btn_read.TabIndex = 1;
+            this.btn_read.Text = "从剪切板\r\n载入字符串";
+            this.btn_read.UseVisualStyleBackColor = true;
+            this.btn_read.Click += new System.EventHandler(this.btn_read_Click);
+            // 
+            // lab_title
+            // 
+            this.lab_title.AutoSize = true;
+            this.lab_title.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lab_title.Location = new System.Drawing.Point(10, 24);
+            this.lab_title.Name = "lab_title";
+            this.lab_title.Size = new System.Drawing.Size(138, 28);
+            this.lab_title.TabIndex = 3;
+            this.lab_title.Text = "字符串分析器";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 635);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(725, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "注：工具的用途主要是用来分析字符串，找出隐藏字符，查看编码等功能，为了效率只支持1000个字符的处理，超出部分不做任何操作。";
+            // 
+            // lab_count
+            // 
+            this.lab_count.AutoSize = true;
+            this.lab_count.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lab_count.ForeColor = System.Drawing.Color.Red;
+            this.lab_count.Location = new System.Drawing.Point(14, 597);
+            this.lab_count.Name = "lab_count";
+            this.lab_count.Size = new System.Drawing.Size(0, 20);
+            this.lab_count.TabIndex = 5;
+            // 
+            // frm_main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 662);
+            this.Controls.Add(this.lab_count);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lab_title);
+            this.Controls.Add(this.btn_read);
+            this.Controls.Add(this.pannel_text);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(935, 700);
+            this.MinimumSize = new System.Drawing.Size(935, 700);
+            this.Name = "frm_main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "字符分析器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel pannel_text;
+        private System.Windows.Forms.Button btn_read;
+        private System.Windows.Forms.Label lab_title;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolTip tip_text;
+        private System.Windows.Forms.Label lab_count;
+    }
+}
+
---
+++ TextAnalysis/frm_main.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace TextAnalysis
+{
+    public partial class frm_main : Form
+    {
+        public frm_main()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_read_Click(object sender, EventArgs e)
+        {
+            //重置标题
+            lab_title.Text = "字符串分析器";
+
+            try
+            {
+                //获取剪切板数据
+                var text = Clipboard.GetText();
+                var arr = text.ToCharArray();
+
+                if (arr.Length == 0)
+                {
+                    return;
+                }
+
+                //处理一下统计
+                var count = string.Format("字符串长度：{0}，字节数组长度：{1} ，其中【\\0】字符：{2} 个，【\\r】字符：{3} 个，【\\n】字符：{4} 个", text.Length, arr.Length, Regex.Matches(text, @"\0").Count, Regex.Matches(text, @"\r").Count, Regex.Matches(text, @"\n").Count);
+                lab_count.Text = count;
+
+                //清空原有的字符
+                pannel_text.Controls.Clear();
+
+                foreach (var t in arr)
+                {
+                    var lb = new LinkLabel();
+                    lb.Text = t.ToString();
+                    lb.BorderStyle = BorderStyle.FixedSingle;
+                    lb.Size = new System.Drawing.Size(30, 30);
+                    lb.Font = new Font("宋体", 12F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(134)));
+                    lb.Margin = new System.Windows.Forms.Padding(0, 0, 5, 10);
+                    lb.Padding = new System.Windows.Forms.Padding(5, 5, 5, 5);
+                    lb.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+                    lb.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+                    lb.Click += new System.EventHandler(TextClick);
+                    pannel_text.Controls.Add(lb);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("错误，无法获取信息，错误如下：" + ex.ToString());
+            }
+        }
+
+        //处理点击后的事件
+        private void TextClick(object sender, EventArgs e)
+        {
+            var lab = sender as LinkLabel;
+            var ts = lab.Text.ToCharArray();
+
+            var ascii = (int)ts[0];
+            var unicode = "\\u" + ((int)ts[0]).ToString("x");
+
+            var hint = string.Format("字符：【{0}】，ASCII编码：【{1}】，Unicode编码：【{2}】", lab.Text, ascii, unicode);
+
+            //打印
+            lab_title.Text = hint;
+        }
+    }
+}
+
+/**
+特殊控制字符说明
+NUL  空	VT   垂直制表	SYN  空转同步
+SOH  标题开始	FF   走纸控制	ETB  信息组传送结束
+STX  正文开始	CR   回车	CAN  作废
+ETX  正文结束	SO   移位输出	EM   纸尽
+EOY  传输结束	SI   移位输入	SUB  换置
+ENQ  询问字符	DLE  空格	ESC  换码
+ACK  承认	DC1  设备控制1	FS   文字分隔符
+BEL  报警	DC2  设备控制2	GS   组分隔符
+BS   退一格	DC3  设备控制3	RS   记录分隔符
+HT   横向列表	DC4  设备控制4	US   单元分隔符
+LF   换行	NAK  否定	DEL  删除
+ * 
+ **/
